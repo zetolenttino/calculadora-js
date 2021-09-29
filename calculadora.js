37287e6fb4f6984076b216d81ddd3e12ceb7483b
zetolenttinopc <zetolenttino@gmail.com>
2021-09-29 17:09:34 -0300

    retirando new Function, implementeando calculos manualmente

--- calculadora.js
+++ calculadora.js
@@ -1,63 +1,20 @@
-/*function preencher(num) {
-    if (typeof gvisor == 'undefined') {
-        document.calcform.visor.value = '';
+var parteA = undefined
+var operador = undefined
+var parteB = undefined
+
+function preencher(texto) {
+    if ('0123456789,'.includes(texto)) {
+        const valor = document.calcform.visor.value += texto
+        if (operador) // operador !== undefined ou null ou 0 ou false ou ''
+            parteB = valor;
+        else
+            parteA = valor;
+    } else if (texto === '+') {
+        operador = texto
+        document.calcform.visor.value = ''
+    } else if (texto === '=') {
+        calcParse()
     }
-    document.calcform.visor.value = document.calcform.visor.value + num;
-
-    gvisor = 1;
-}
-
-function limparCampo() {
-    document.calcform.visor.value = '';
-}
-
-function calculoOperador(operador, valor1, valor2) {
-    if (operador == 'soma') {
-        var res = valor1 + valor2;
-        //var res = parseFloat(valor1) + parseFloat(valor2);
-    } else if (operador == 'subtracao') {
-        var res = valor1 - valor2;
-    } else if (operador == 'divisao') {
-        var res = valor1 / valor2;
-    } else if (operador == 'multiplicacao') {
-        var res = valor1 * valor2;
-    }
-
-    return (res);
-}
-
-function calcParse(operador) {
-
-    var valor = document.calcform.visor.value;
-    delete gvisor;
-
-    if (typeof goperador != 'undefined' && operador == 'resultado') {
-        gres = calculoOperador(operador, gres, res);
-        document.calcform.visor.value = gres;
-
-        delete operador;
-        delete gres;
-
-        return (0);
-
-    }
-
-    if (typeof gvalor != 'undefined') {
-        gres = calculoOperador(operador, gres, res);
-        goperador = operador;
-        document.calcform.visor.value = gres;
-
-    } else {
-        gres = res;
-        goperador = operador;
-    }
-}*/
-
-var valor;
-var resultado;
-
-function preencher(num) {
-    valor = document.calcform.visor.value += num;
 }
 
 function limparCampo() {
@@ -65,13 +22,11 @@ function limparCampo() {
 }
 
 function calcParse() {
-    const expressao = valor.replace(/,/g, '.') // 1+2
-    const expressaoComReturn = 'return ' + expressao // return 1+2
-    const funcaoQueCalculaAExpressao = new Function([], expressaoComReturn) // ~é como se declarasse a funcao abaixo
-    resultado = funcaoQueCalculaAExpressao();
+    // const expressao = parteA.replace(/,/g, '.') + operador + parteB.replace(/,/g, '.')
+    // const expressaoComReturn = 'return ' + expressao // return 1+2
+    // const funcaoQueCalculaAExpressao = new Function([], expressaoComReturn) // ~é como se declarasse a funcao abaixo
+    // resultado = funcaoQueCalculaAExpressao();
+    let resultado
+    if (operador === '+') resultado = parseFloat(parteA) + parseFloat(parteB)
     document.calcform.visor.value = resultado.toLocaleString('pt-BR');
 }
-
-// function funcaoQueCalculaAExpressao() {
-//     return 1+2   // expressaoComReturn
-// }
