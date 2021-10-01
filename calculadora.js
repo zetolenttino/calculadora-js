4f7aa1174a31540165de4ea9298404c5a0bf191c
zetolenttino <zetolenttino@gmail.com>
2021-10-01 17:57:17 -0300

    correção do erro de merge

--- calculadora.js
+++ calculadora.js
@@ -4,60 +4,37 @@ var parteB = undefined
 
 function preencher(texto) {
     if ('0123456789,'.includes(texto)) {
-        document.calcform.visor.value += texto // concatena no visor
+        const valor = document.calcform.visor.value += texto
         if (operador) // operador !== undefined ou null ou 0 ou false ou ''
-            parteB = document.calcform.visor.value;
+            parteB = valor;
         else
-            parteA = document.calcform.visor.value;
-    } else if (texto === '+') {
-        operador = texto
-        document.calcform.visor.value = '' <<
-            << << < HEAD
-    } else if (texto === '-') {
+            parteA = valor;
+    } else if (texto === '+' || operador === '-' || operador === '*' || operador === '/') {
         operador = texto
         document.calcform.visor.value = ''
-    } else if (texto === '/') {
-        operador = texto
-        document.calcform.visor.value = ''
-    } else if (texto === '*') {
-        operador = texto
-        document.calcform.visor.value = ''
-    } else if (texto === '=') { ===
-        === =
-    } else if (texto === '-') {
-        operador = texto
-        document.calcform.visor.value = ''
-    } else if (texto === '*') {
-        operador = texto
-        document.calcform.visor.value = ''
-    } else if (texto === '/') {
-        operador = texto
-        document.calcform.visor.value = ''
-    } else if (texto === '=') { >>>
-        >>> > 80 cecbc(implementando AC no teste, e arrumando bugde AC)
-        calcParse()
-        document.calcform.visor.value = '';
+        }
     }
-}
+
 
 function limparCampo() {
-    document.calcform.visor.value = ''; <<
-    << << < HEAD
-    delete parteB;
-    delete parteA;
-    delete operador;
-    delete resultado; ===
-    === =
-    parteA = undefined
+    document.calcform.visor.value = '';
     parteB = undefined
-    operador = undefined >>>
-        >>> > 80 cecbc(implementando AC no teste, e arrumando bugde AC)
+    parteA = undefined
+    operador = undefined
 }
 
 function calcParse() {
     let resultado
-    if (operador === '+' || operador === '-' || operador === '*' || operador === '/') {
+    if (operador === '+'){
         resultado = parseFloat(parteA.replace(/,/g, '.')) + parseFloat(parteB.replace(/,/g, '.'))
+    }else if (operador === '-'){
+        resultado = parseFloat(parteA.replace(/,/g, '.')) - parseFloat(parteB.replace(/,/g, '.'))
+    }else if (operador === '/'){
+        resultado = parseFloat(parteA.replace(/,/g, '.')) / parseFloat(parteB.replace(/,/g, '.'))
+    }else if (operador === '*'){
+        resultado = parseFloat(parteA.replace(/,/g, '.')) * parseFloat(parteB.replace(/,/g, '.'))
     }
+
     document.calcform.visor.value = resultado.toLocaleString('pt-BR');
+  
 }
